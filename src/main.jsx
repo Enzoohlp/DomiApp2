66e329e111159c479b181b3e92c081364c012eae
Enzoohlp <tm.enzomonsalve@gmail.com>
2026-06-04 17:40:59 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>Domi App funcionando 🚀</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
